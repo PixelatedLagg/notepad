383e9e8eca84dd511db7ae5e31e875262f9cf2d1
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-03-13 23:31:43 -0400

    init

---
+++ main.js
@@ -0,0 +1,39 @@
+var autocorrect = false;
+var lineIndicator = document.getElementById("line");
+var area = document.getElementById("area");
+
+function LineChange()
+{
+    var lines = area.value.substr(0, area.selectionStart).split("\n");
+    lineIndicator.innerHTML = `(${lines.length}, ${lines[lines.length - 1].length})`;
+}
+
+function Autocorrect()
+{
+    if (autocorrect)
+    {
+        area.setAttribute("spellcheck", "false");
+        autocorrect = false;
+    }
+    else
+    {
+        area.setAttribute("spellcheck", "true");
+        autocorrect = true;
+    }
+}
+
+function Clear()
+{
+    let clear = window.confirm('Are you sure you want to clear the notepad?');
+    if (clear)
+    {
+        area.value = "";
+    }
+}
+
+function Save()
+{
+    var blob = new Blob([area.value], { type: "text/plain;charset=utf-8" });
+    let name = prompt('Name of the file:', "");
+    saveAs(blob, `${name}.txt`);
+}
